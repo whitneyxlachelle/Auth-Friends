67e91bb59d893be6c5af61dcd9238c922fdc1ca2
whitneyxlachelle <whiggins16@yahoo.com>
2020-03-17 17:45:30 -0500

    first post request

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import LoginForm from './components/LoginForm';
 
--- friends/src/components/LoginForm.js
+++ friends/src/components/LoginForm.js
@@ -1,20 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const LoginForm = () => {
-    const [credentials, setCredentials] = useState({});
+    //console.log(props);
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: ""
+    });
+    //const [isLoading, setIsLoading] = useState(false);
+
+    const handleChange = event => {
+        event.preventDefault();
+        setCredentials({
+            ...credentials,
+            [event.target.name]: event.target.value,
+        })
+    }
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        axios
+        .post("/api/login", credentials)
+        .then(res => {
+            console.log(res);
+            localStorage.setItem("token", res.data.token)
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    };
 
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit} >
                 <input
                     type="text"
                     name="username"
+                    value={credentials.username}
+                    onChange={handleChange}
+                    placeholder="Username"
                 />
                 <input
                     type="password"
                     name="password"
+                    value={credentials.password}
+                    onChange={handleChange}
+                    placeholder="Password"
                 />
-                <button>Log In</button>
+                <button className="button">Log In</button>
             </form>
         </div>
     )
